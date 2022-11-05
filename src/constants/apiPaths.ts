8f42699f947e175b3b415f30ac95a8dd4149e95d
Sergey <sergeishmaro@gmail.com>
2022-11-06 02:36:30 +0300

    Task-9: Change api to elasticbeanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   import: "https://adhgy8sjn5.execute-api.eu-west-1.amazonaws.com",
   product: "https://6b1jcnr1b2.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://bxaw3uw8vg.execute-api.eu-west-1.amazonaws.com/dev/api",
+  cart: "http://sergeyshmaro-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
