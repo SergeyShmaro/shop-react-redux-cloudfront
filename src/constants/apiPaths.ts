25ca164f4fd65e5d5d04f6c259033ac05e624ea1
Sergey <sergeishmaro@gmail.com>
2022-11-06 18:05:07 +0300

    Task-9: Change api to API gateway with proxy to elasticbeanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   import: "https://adhgy8sjn5.execute-api.eu-west-1.amazonaws.com",
   product: "https://6b1jcnr1b2.execute-api.eu-west-1.amazonaws.com",
-  cart: "http://sergeyshmaro-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://rd5fhe73xc.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
